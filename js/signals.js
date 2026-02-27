1886621e2dd77b929580c160d002e7f9d59b4bd8
haind-cyber <hai.nguyenduc1989@gmail.com>
2026-02-27 11:06:14 +0700

    Release v1.11.20

--- js/signals.js
+++ js/signals.js
@@ -1,6 +1,6 @@
 // ============================================
 // Bitcoin PeakDip Early Warning System - Signals
-// Version: 1.11.19 - UPDATED - Signal Details Floating & Layout
+// Version: 1.11.20 - UPDATED - Signal Details Floating & Layout
 // ============================================
 
 // ========== APP CONFIGURATION ==========
